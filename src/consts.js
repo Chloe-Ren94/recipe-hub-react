aabf9f1ed1bbc52b04adaa875663bcc9deeae8b7
Chloe-Ren94 <ren.jie1@northeastern.edu>
2022-01-20 00:34:57 -0800

    update api url

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 //const API_KEY = '7fb5aa48707244af9237ed4ac3fbff7f';
 const API_KEY = '9f4582a7124d4d9daf20c34c259a8717';
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://recipe-hub-node.herokuapp.com/api';
 
 module.exports = {
     API_KEY, API_URL
